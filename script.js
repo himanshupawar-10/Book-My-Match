84cba8766804d013ac17e8998f4baf490ece16c0
Himanshu <hmpawar10@gmail.com>
2020-03-19 23:54:32 +0530

    updated cuntact us section

--- script.js
+++ script.js
@@ -3,7 +3,3 @@ var y= n.getFullYear();
 var m= n.getMonth();
 var d= n.getDate();
 document.getElementById("todate").innerHTML= "("+d + "/" + m + "/" + y+")";
-
-function alert(){
-	alert("Querie Successfully Submitted")
-}
