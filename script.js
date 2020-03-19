0ab45201d33672746a7b6d4fc3e5efb7fe015ab8
Himanshu <hmpawar10@gmail.com>
2020-03-19 23:01:09 +0530

    many new adds

---
+++ script.js
@@ -0,0 +1,9 @@
+var n= new Date();
+var y= n.getFullYear();
+var m= n.getMonth();
+var d= n.getDate();
+document.getElementById("todate").innerHTML= "("+d + "/" + m + "/" + y+")";
+
+function alert(){
+	alert("Querie Successfully Submitted")
+}
